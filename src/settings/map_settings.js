98d2081cf652230fb21e8f8a8049f62559e9d013
Caleb Robinson <calebrob6@gmail.com>
2024-10-11 22:58:02 -0700

    Update map_settings.js

--- src/settings/map_settings.js
+++ src/settings/map_settings.js
@@ -6,18 +6,18 @@ export let mapSettings = {
 	azureMapsAuth: {
 		
 		//Option 1: Use Azure Active Directory authentication for secure access to Azure Maps.
-		authType: "anonymous",
-		clientId: "d069e722-70c3-4dd6-8532-a6f4b18c9bfb", //Your Azure Maps client id for accessing your Azure Maps account.
-		getToken: function (resolve, reject, map) {
-			//URL to your authentication service that retrieves an Azure Active Directory Token.
-			var tokenServiceUrl = "https://spatial-annotation-tool-maps-auth2.azurewebsites.net/api/GetAzureMapsToken";
+		// authType: "anonymous",
+		// clientId: "d069e722-70c3-4dd6-8532-a6f4b18c9bfb", //Your Azure Maps client id for accessing your Azure Maps account.
+		// getToken: function (resolve, reject, map) {
+		// 	//URL to your authentication service that retrieves an Azure Active Directory Token.
+		// 	var tokenServiceUrl = "https://spatial-annotation-tool-maps-auth2.azurewebsites.net/api/GetAzureMapsToken";
 
-			fetch(tokenServiceUrl).then(r => r.text()).then(token => resolve(token));
-		}
+		// 	fetch(tokenServiceUrl).then(r => r.text()).then(token => resolve(token));
+		// }
 
 		//Option 2: Use an Azure Maps key. Get an Azure Maps key at https://azure.com/maps. NOTE: The primary key should be used as the key.
-		/*authType: "subscriptionKey",
-		subscriptionKey: "<Your Azure Maps Key>"*/
+		authType: "subscriptionKey",
+		subscriptionKey: "no key"
 	},
 
     //A URL to a CORs enabled proxy service that can be leveraged for cross domain requests. Cross domain URL would be appended to this proxy service URL. 
